e6f1c26cb4c2650a5baae6528c9c3a9f732c133a
syfj123 <padhy.shash@gmail.com>
2026-06-09 11:45:22 -0400

    finished task 3. simple database in the form of a dictionary, accessing a value by calling the dictionary at the specific key.

---
+++ task3.lua
@@ -0,0 +1,12 @@
+-- database
+
+local inventory={
+    fruits=0,
+    vegetables=23,
+    chicken=8,
+    crawfish=3,
+    laptops=0,
+    garbage_bags=100
+}
+
+print("there are "..inventory["vegetables"].." vegetables available in the inventory, woohoo!")
